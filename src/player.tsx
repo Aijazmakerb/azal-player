58ac38198eb59b4dd13883d474596a71814cf598
Aijazmakerb <786aijazusmaan@gmail.com>
2024-06-09 20:32:40 +0530

    init

--- src/player.tsx
+++ src/player.tsx
@@ -1,6 +1,6 @@
 import '@vidstack/react/player/styles/base.css';
 
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import {
   isHLSProvider,
@@ -44,11 +44,50 @@ export function Player() {
     // ...
   }
 
+  interface Source {
+    file: string;
+    label: string;
+    default: boolean;
+    type: string;
+  }
+
+  interface Track {
+    // Define the properties for track if there are any
+  }
+
+  interface Advertising {
+    // Define the properties for advertising if there are any
+  }
+
+  interface VideoData {
+    source: Source[];
+    source_bk: Source[];
+    track: Track[];
+    advertising: Advertising[];
+    linkiframe: string;
+  }
+
+  const [data, setData] = useState<VideoData>();
+
+  useEffect(() => {
+    async function fetchData() {
+        try {
+            const response = await fetch('https://proxy.gojo.live/?url=https://azal-api.vercel.app/sources/exhuma-2024-episode-1.html');
+            const result: VideoData = await response.json();
+            setData(result);
+        } catch (error) {
+            console.error('Error fetching the data:', error);
+        }
+    }
+
+    fetchData();
+}, []);
+
   return (
     <MediaPlayer
       className="w-full aspect-video bg-slate-900 text-white font-sans overflow-hidden rounded-md ring-media-focus data-[focus]:ring-4"
-      title="Sprite Fight"
-      src="https://files.vidstack.io/sprite-fight/720p.mp4"
+      title="Exhuma"
+      src={data?.source[0]?.file}
       crossOrigin
       playsInline
       onProviderChange={onProviderChange}
@@ -58,12 +97,12 @@ export function Player() {
       <MediaProvider>
         <Poster
           className="absolute inset-0 block h-full w-full rounded-md opacity-0 transition-opacity data-[visible]:opacity-100 object-cover"
-          src="https://files.vidstack.io/sprite-fight/poster.webp"
+          src="https://image.tmdb.org/t/p/original/m8k1rFB68IZmIUoJfpnboY4taVg.jpg"
           alt="Girl walks into campfire with gnomes surrounding her friend ready for their next meal!"
         />
-        {textTracks.map((track) => (
+        {/* {textTracks.map((track) => (
           <Track {...track} key={track.src} />
-        ))}
+        ))} */}
       </MediaProvider>
 
       <VideoLayout thumbnails="https://files.vidstack.io/sprite-fight/thumbnails.vtt" />
